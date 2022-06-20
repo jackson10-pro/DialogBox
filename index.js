84f410313fe14547ffcda9d292201d9756b75c9a
react.solvit <react.solvit@gmail.com>
2022-06-20 10:27:28 +0200

    adding js code to add images on the DOM

--- index.js
+++ index.js
@@ -3,20 +3,46 @@ const outputBox = document.querySelector('output');
 const confirmResult = document.querySelector('#confirmBtn');
 const selectedItem = document.querySelector('select');
 
+
 const avatarBtn = document.getElementById('add_new_avatar');
 
-// clickHereBtn.addEventListener('click', function onOpen() {
-//     dialog.showModal();
-// });
 
 avatarBtn.addEventListener('click', function onOpen() {
     dialog.showModal();
-})
+});
 
-// selectedItem.addEventListener('change', function onSelect(e) {
-//     confirmResult.value = selectedItem.value;
-// });
 
-// dialog.addEventListener('close', function onClose() {
-//     outputBox.value = dialog.returnValue + "you closed the dialog box"
-// });
+const avatarFormElement = document.querySelector("#get_avatar_image_form");
+
+function formSubmitted(event){
+    const fileInput = avatarFormElement.querySelector("input[name=userimage]");
+    const files = fileInput.files;
+    if(files.length == 0 ) {
+        console.log("No file submitted");
+        return;
+    }
+    const image = files[0];
+   const imageUrl = URL.createObjectURL(image);
+   addImageToTheDOM(imageUrl);
+
+}
+function addImageToTheDOM(imageURL){
+    const button = document.createElement("button");
+    button.classList.add("avatar-img-btn");
+    const closeIcon = document.createElement("ion-icon");
+    closeIcon.classList.add("close-avatar-icon");
+    closeIcon.name = "close-outline";
+
+    button.appendChild(closeIcon);
+    
+    const imageElt = document.createElement("img");
+    imageElt.src = imageURL;
+    imageElt.classList.add("avatar-img");
+
+    button.appendChild(imageElt);
+    const addAvatarButton = document.querySelector("#add_new_avatar");
+    document.body.insertBefore(button, addAvatarButton);
+
+}
+
+avatarFormElement.addEventListener("submit", formSubmitted);
